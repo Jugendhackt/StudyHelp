546259e3f3b12be9d5fb150fb7e96be9e2b1ad4a
ePirat <epirat07@gmail.com>
2013-09-07 15:42:58 +0200

    New Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudyHelp::Application.config.secret_key_base = '81fefe7456c1363d62e47a0a360cfcc14a05525f819a89019a12cd721c0f167c786ac4395e3c91e846100e5a4bbd1bdac4a3a27ba2da34c8141701ed4d49b025'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StudyHelp::Application.config.session_store :cookie_store, key: '_StudyHelp_session'
